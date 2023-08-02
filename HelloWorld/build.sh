8ddafb485ae82aaf9860e7fee5f53840693dfc4b
JamJestJam <marekti012@gmail.com>
2023-08-02 19:35:20 +0200

    Upgrade create

--- HelloWorld/build.sh
+++ HelloWorld/build.sh
@@ -1,3 +1,5 @@
+cd $(dirname $0)
+
 # gcc -std=c++20 -fmodules-ts -xc++-system-header iostream
 gcc -std=c++20 -fmodules-ts -Wall -Wextra -Werror -pedantic -Wconversion -lstdc++ -ggdb3 -O3 main.cpp
 export LD_LIBRARY_PATH="/usr/local/lib64/:$LD_LIBRARY_PATH"
